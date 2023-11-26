72a8a14585612ce56d706a841ca3b15f387c411f
zys91 <919129850@163.com>
2023-11-26 17:29:46 +0800

    Update some dependences

--- jd/usual/jd_price.js
+++ jd/usual/jd_price.js
@@ -21,7 +21,7 @@ cron "55 12 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/j
 const $ = new Env('京东价保');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./function/jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const jsdom = $.isNode() ? require('jsdom') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message, allMessage = '';
@@ -268,7 +268,7 @@ function taskUrl(functionId, body, h5st = '', time = Date.now()) {
       "Content-Type": "application/x-www-form-urlencoded",
       "Origin": "https://msitepp-fm.jd.com",
       "Accept-Language": "zh-CN,zh-Hans;q=0.9",
-      "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./function/USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
+      "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
       "Referer": "https://msitepp-fm.jd.com/",
       "Accept-Encoding": "gzip, deflate, br",
       "Cookie": cookie
